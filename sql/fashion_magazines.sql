250469413eb5ecf05a686f224f141886ff3c16c9
Lance Biddle <lancebiddle@Lances-MacBook-Pro.local>
2024-10-27 19:53:04 -0400

    uploading homework

--- sql/fashion_magazines.sql
+++ sql/fashion_magazines.sql
@@ -1,1 +1,9 @@
 -- Add your sql here
+SELECT customer_name AS Customer, PRINTF("$%.2f",sum(price_per_month * subscription_length)) AS "Amount Due"
+FROM customers
+JOIN orders, subscriptions
+ON orders.customer_id = customers.customer_id
+AND subscriptions.subscription_id = orders.subscription_id
+AND subscriptions.description = "Fashion Magazine"
+WHERE orders.order_status = "unpaid"
+GROUP BY customers.customer_name;
